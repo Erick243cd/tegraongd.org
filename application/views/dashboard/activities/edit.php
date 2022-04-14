7439289b602b218a7a6918a7753f418eacee3f0a
Erick Banze <erickbanze.develop@gmail.com>
2022-04-14 16:57:57 -0700

    Edit activity file

--- application/views/dashboard/activities/edit.php
+++ application/views/dashboard/activities/edit.php
@@ -1,60 +1,71 @@
-<?php
-$session_data= $this->session->userdata('logged_in');
-$data['id'] = $session_data['id'];
-$data['user_name'] = $session_data['user_name'];
-$data['role_utilisateur'] = $session_data['role_utilisateur'];
-$data['user_image'] = $session_data['user_image'];
-if(empty($data['id'])){
-    redirect(base_url('pages/connexion'));
-}
-?>
-<main>
-	<div class="container-fluid m-5 p-5">
-		<div class="row">
-			<div class="col-3">
-				<div class="">
-					<ul class="list-group">
-						<li class="list-item"><a class="genric-btn primary btn-lg default" href="<?= base_url()?>pages/dashboard">Dashboard</a></li>
-						<div class="ml-2">
-						<li class="list-item"><a class="genric-btn default btn-lg" href="<?= base_url()?>activities/liste">Activités</a></li>
-						<li class="list-item"><a class="genric-btn default btn-lg" href="<?= base_url()?>pages/dashboard">Publier</a></li>
-						<li class="list-item"><a onclick="return confirm('Etes-vous sûr de vous déconnecter');" class="genric-btn default btn-lg" href="<?= base_url()?>pages/logout">Logout</a></li>
-						</div>
-					</ul>
+<!-- start page content -->
+<div class="page-content-wrapper">
+	<div class="page-content">
+		<div class="page-bar">
+			<div class="page-title-breadcrumb">
+				<div class=" pull-left">
+					<div class="page-title"><?= $title ?></div>
 				</div>
-			</div>
-			<div class="col-8">
-				<form class="form-contact contact_form" action="<?= base_url()?>activities/update" method="post" enctype="multipart/form-data">
-			<div class="row">
-			<input type="hidden" name="slug" value="<?= $activity['slug']?>">
-			<div class="col-sm-12">
-				<div class="form-group">
-				<input class="form-control valid" name="title" id="title" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Titre de la publication'" placeholder="Titre de la publication" required="required" value="<?= $activity['title']?>">
-				</div>
-			</div>
-			<div class="col-12">
-			<div class="form-group">
-			<textarea class="form-control w-100" name="body" id="" cols="30" rows="5" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Entrer le Conteny'" placeholder=" Entrer le contenu" required="required"><?= $activity['body']?></textarea>
-			</div>
-			</div>
-
-			<div class="col-sm-6">
-			<div class="form-group">
-				<select class="form-control" name="id_category" required>
-					<option value="<?= $activity['id_category']?>" selected><?= $activity['name']?></option>
-					<option disabled>Séléctionner la catégorie</option>
-					<?php foreach($categories as $row):?>
-						<option value="<?= $row->id_category?>"><?= $row->name?></option>
-					<?php endforeach;?>
-				</select>
-			</div>		
+				<ol class="breadcrumb page-breadcrumb pull-right">
+					<li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href="<?= site_url('pages/dashboard') ?>">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
+					</li>
+					<li><a class="parent-item" href="#"><?= $title ?></a>&nbsp;<i class="fa fa-angle-right"></i>
+					</li>
+					<li class="active"><?= $subtitle ?></li>
+				</ol>
 			</div>
 		</div>
-			<div class="form-group mt-3">
-			<button type="submit" class="genric-btn primary btn-fixed" onclick="return confirm('Etes-vous sûr de modifier ?')">Mettre à jour</button>
+
+		<div class="row">
+			<!-- Quick Mail start -->
+			<div class="col-lg-12 col-md-12 col-sm-12 col-12">
+				<div class="inbox">
+					<div class="card">
+						<div class="card-body no-padding height-9">
+							<div class="inbox-body">
+								<div class="mail-list">
+									<div class="compose-mail">
+										
+										<?= form_open('activities/update/'.$activity['slug']) ?>
+
+								
+										<div class="email-form">
+											<label for="subject" class="">Titre :</label>
+											<input type="text" tabindex="1" id="subject" name="post_title" class="form-control itemField" value="<?= $activity['title']; ?>" required>
+										</div>
+										<span class="text-danger"><?= form_error('post_title'); ?>
+
+											<div class="form-group">
+												
+												<select class="form-select" name="category">
+													<option value="">Selectionner la catégorie</option>
+													<?php foreach ($categories as $row) : ?>
+														<option value="<?= $row->id_category ?>" <?= set_select('category', $row->id_category, TRUE); ?> ><?= $row->name ?></option>
+													<?php endforeach ?>
+												</select>
+											</div>
+											<span class="text-danger"><?= form_error('category'); ?>
+												<div class="mt-4">
+													<textarea name="post_description" id="formsummernote" cols="30" rows="5" required>
+														<?= $activity['body']?>	
+													</textarea>
+												</div>
+
+												<div class="box-footer clearfix">
+													<button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect m-b-10 btn-warning pull-right">
+														Mettre à jour <i class="fa fa-paper-plane-o"></i>
+													</button>
+												</div>
+											</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-			</form>
-			</div>
+			<!-- Quick Mail end -->
 		</div>
 	</div>
-</main>
+</div>
+<!-- end page content -->
